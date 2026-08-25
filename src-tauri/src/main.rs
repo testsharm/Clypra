e38999b8e8f6ee15daf90f802d7ad3d867845947
testsharm <notmorethanititsfinal@gmail.com>
2026-08-25 07:56:26 +0000

    Final WiX fix: use fragmentPaths + componentRefs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,20 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    use std::env;
-    use std::path::PathBuf;
-
-    // Add the resources folder to the DLL search path
-    if let Ok(exe_path) = env::current_exe() {
-        if let Some(exe_dir) = exe_path.parent() {
-            let resources_dir = exe_dir.join("resources");
-            if resources_dir.exists() {
-                let current_path = env::var("PATH").unwrap_or_default();
-                let new_path = format!("{};{}", resources_dir.display(), current_path);
-                env::set_var("PATH", &new_path);
-            }
-        }
-    }
-
     tauri_app_lib::run()
 }
